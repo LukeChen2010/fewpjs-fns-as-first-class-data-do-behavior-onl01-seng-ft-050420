a05af6fde81fafadfdfb1271e93f0eb70de8d75f
Luke Chen <luke.chen.2010@gmail.com>
2020-07-19 14:06:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function handleClick(e) {
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
 
-function greet(timeStr) 
+function greet(time) 
 {
-  let hr = parseInt(timeStr, 10);
-  if ( hour < 12) return "Good Morning"
+  let hr = time.split(":")
+  if ( hr < 12) return "Good Morning"
   if ( hour > 17) return "Good Evening"
   return "Good Afternoon"
 }
